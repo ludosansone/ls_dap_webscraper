8ae2de62d1c732b3f87969c0e581c4c4611b4216
Ludovic SANSONE <libreaudio63@gmail.com>
2022-03-17 10:08:41 +0100

    Ajout de la déclaration de la fonction get_soup dans le fichier extraction.py

--- extraction.py
+++ extraction.py
@@ -1,2 +1,28 @@
 import requests
 from bs4 import BeautifulSoup as bs
+
+def get_page(url):
+    """
+        On récupère le contenu de la page dont l'URL est placé en argument
+    """
+
+    try:
+        response = requests.get(url)
+        return response.text
+    except:
+        return False
+#
+
+def get_soup(html_page):
+    """
+        On converti la page html en un objet BeautifulSoup
+        Par ici la bonne soupe
+    """
+
+    try:
+        soup = bs(html_page, 'html.parser')
+        return soup
+    except:
+        return False
+#
+
